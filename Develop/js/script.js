b1585956d6fde7aea609929b05157f938da33a5b
brittanieb <brittanie.boyko@gmail.com>
2019-12-15 22:20:54 -0500

    grab desciption value and hour and save it in local storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+
+    $(".button-save").on("click", function() {
+        var description = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
+        localStorage.setItem(description, hour);
+    });
+
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
