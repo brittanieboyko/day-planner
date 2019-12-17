e03cbe39b62c33217acb1355ece008b2f3fc0f84
brittanieb <brittanie.boyko@gmail.com>
2019-12-17 08:12:32 -0500

    pushed description index and description into array of description objects to set in local storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
 
+    var descriptions = [];
+
+    function storeDescriptions() {
+        localStorage.setItem("descriptions", descriptions);
+    }
+
     $(".button-save").on("click", function() {
-        var description = $(this).siblings(".description").val();
-        var hour = $(this).parent().attr("id");
-        localStorage.setItem(description, hour);
+        var description = $(this).siblings(".description").val().trim();
+        var index = $(this).parent().data("index");
+
+        if (description === "") {
+            return
+        }
+        descriptions.push({description: description, index : index});
+
+        storeDescriptions();
     });
 
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
