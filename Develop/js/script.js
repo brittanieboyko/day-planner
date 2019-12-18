da89860a64dea983778ae6bab5675934f4d20fda
brittanieb <brittanie.boyko@gmail.com>
2019-12-18 00:53:05 -0500

    created array of hour values. set those values to hours in moment.js and evaluated for  past/present/future

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
 
     var descriptions = [];
+    var timeBlocks = [];
 
+    storeHours();
+    
     function storeDescriptions() {
         localStorage.setItem("descriptions", descriptions);
     }
 
-    $(".button-save").on("click", function() {
+    function storeHours() {
+        $('.time-block').each(function(i,element) {
+            timeBlocks.push($(element).attr('id'));
+        });
+        determineHour();
+    }
+
+    function determineHour() {
+        timeBlocks.forEach(function(hour) {
+            var timeSlot = moment().hour(hour);
+            if ((moment().isAfter(timeSlot))) {
+                $(".time-block").find("#("+hour+")").addClass('past');
+            } else if ((moment().isBefore(timeSlot))) {
+                $(".container").find("#"+hour).addClass('future');
+            } else {
+                $(".container").find("#"+hour).addClass('present');
+            }
+        })
+    }
+
+    $(".button-save").on("click", function(event) {
+        event.preventDefault();
         var description = $(this).siblings(".description").val().trim();
         var index = $(this).parent().data("index");
 
